0f4a2a8821b2a47b6c63224705afe0fdc56e2f46
Reveigh <126974820+Reveigh@users.noreply.github.com>
2023-11-01 19:04:33 -0400

    update main

---
+++ Install Chrome.ps1
@@ -0,0 +1,8 @@
+﻿$url = "https://github.com/Reveigh/share/raw/Client/ChromeStandaloneSetup64.exe"
+$outputPath = "C:\ChromeStandaloneSetup64.exe"
+
+# Download the executable from the GitHub repository to C:\
+Invoke-WebRequest -Uri $url -OutFile $outputPath
+
+# Execute the downloaded executable
+Start-Process -FilePath $outputPath
