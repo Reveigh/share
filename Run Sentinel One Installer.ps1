934c5f0ffa0302b91da04639ed0b4a9b75d666b6
Reveigh <126974820+Reveigh@users.noreply.github.com>
2023-11-01 21:13:33 -0400

    Update Run Sentinel One Installer.ps1

--- Run Sentinel One Installer.ps1
+++ Run Sentinel One Installer.ps1
@@ -7,14 +7,6 @@ $tokenCode = Read-Host "Enter the token code"
 # Download the executable from the GitHub repository to the specified local path
 Invoke-WebRequest -Uri $url -OutFile $outputPath
 
-# Check if the download was successful
-if (Test-Path $outputPath) {
-    Write-Host "Download completed. Running the executable..."
-
-    # Execute the downloaded executable with the arguments and token code
-    $process = Start-Process -FilePath $outputPath -ArgumentList $arguments, $tokenCode -NoNewWindow -PassThru
-
-    # Wait for the executable to finish and show the status
+# Wait
     $process.WaitForExit()
-
     Write-Host "Installation completed successfully."
