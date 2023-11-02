7decf757ee89e645042951a19bab9ff2fb47ca4a
Reveigh <126974820+Reveigh@users.noreply.github.com>
2023-11-01 22:10:05 -0400

    Update Run Sentinel One Installer.ps1

--- Run Sentinel One Installer.ps1
+++ Run Sentinel One Installer.ps1
@@ -7,27 +7,33 @@ $outputPath = 'C:\SentinelOneInstaller.exe'
 # Prompt the user to enter the token code
 $tokenCode = Read-Host "Enter the token code"
 
-# Download the executable from the GitHub repository to the specified local path
-Invoke-WebRequest -Uri $url -OutFile $outputPath
-
-# Check if the download was successful
+# Check if the file already exists
 if (Test-Path $outputPath) {
-    Write-Host "Download completed. Starting the installation..."
-
-    # Create a background job to run the executable with arguments
-    $job = Start-Job -ScriptBlock {
-        param (
-            $outputPath,
-            $tokenCode
-        )
-        & $outputPath -q -t $tokenCode
-    } -ArgumentList $outputPath, $tokenCode
-
-    # Wait for the job to complete
-    Wait-Job $job
-
-    # Remove the completed job
-    Remove-Job $job
+    Write-Host "The executable file already exists at $outputPath."
 } else {
-    Write-Host "Download failed. The executable file was not saved."
+    # Download the executable from the GitHub repository to the specified local path
+    Write-Host "Downloading the executable..."
+    Invoke-WebRequest -Uri $url -OutFile $outputPath
+
+    # Check if the download was successful
+    if (Test-Path $outputPath) {
+        Write-Host "Download completed. Starting the installation..."
+
+        # Create a background job to run the executable with arguments
+        $job = Start-Job -ScriptBlock {
+            param (
+                $outputPath,
+                $tokenCode
+            )
+            & $outputPath -q -t $tokenCode
+        } -ArgumentList $outputPath, $tokenCode
+
+        # Wait for the job to complete
+        Wait-Job $job
+
+        # Remove the completed job
+        Remove-Job $job
+    } else {
+        Write-Host "Download failed. The executable file was not saved."
+    }
 }
