f32ac610a0147c3c4641848aa0c647a7bb710caa
Reveigh <126974820+Reveigh@users.noreply.github.com>
2023-11-01 23:00:49 -0400

    Update Run Sentinel One Installer.ps1

--- Run Sentinel One Installer.ps1
+++ Run Sentinel One Installer.ps1
@@ -4,20 +4,27 @@ $url = 'https://github.com/Reveigh/share/raw/main/SentinelOneInstaller_windows_6
 # Define the local path where you want to save the executable
 $outputPath = 'C:\SentinelOneInstaller.exe'
 
+# Check if the file already exists in the output path
+if (-not (Test-Path $outputPath)) {
+    # File doesn't exist, so download it
+    Write-Host "Downloading the executable..."
+    Invoke-WebRequest -Uri $url -OutFile $outputPath
+
+    # Check if the download was successful
+    if (Test-Path $outputPath) {
+        Write-Host "Download completed. Starting the installation..."
+    } else {
+        Write-Host "Download failed. The executable file was not saved."
+        exit
+    }
+} else {
+    Write-Host "The file already exists in the output path."
+}
+
 # Prompt the user to enter the token code
 $tokenCode = Read-Host "Enter the token code"
 
-# Download the executable from the GitHub repository to the specified local path
-Invoke-WebRequest -Uri $url -OutFile $outputPath
+# Run the downloaded executable with arguments and token code
+Start-Process -FilePath $outputPath -ArgumentList "-q", "-t", $tokenCode
 
-# Check if the download was successful
-if (Test-Path $outputPath) {
-    Write-Host "Download completed. Starting the installation..."
-
-    # Run the downloaded executable with arguments and token code
-    Start-Process -FilePath $outputPath -ArgumentList "-q", "-t", $tokenCode
-    
-    Write-Host "Installation completed."
-} else {
-    Write-Host "Download failed. The executable file was not saved."
-}
+Write-Host "Installation completed."
